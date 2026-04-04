26288cfbfd1345f09067bc84debabd452b560168
Ido Kanner <idok@linesip.com>
2026-04-04 11:04:05 +0300

    changd the colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    "loctvl842/monokai-pro.nvim",
-    lazy = false,
-    priority = 1000, -- load before everything else
-    opts = {
-      filter = "pro",
-    },
-  },
+	{
+		"loctvl842/monokai-pro.nvim",
+		lazy = false,
+		priority = 1000, -- load before everything else
+		opts = {
+			filter = "pro",
+		},
+	},
 
-  -- Tell LazyVim to use this colorscheme instead of tokyonight
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "monokai-pro",
-    },
-  },
+	-- Tell LazyVim to use this colorscheme instead of tokyonight
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "monokai-pro-machine",
+		},
+	},
 }
